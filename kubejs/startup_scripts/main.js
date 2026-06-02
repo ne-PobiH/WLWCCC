91a1c8d4a7313408bf0e25ebc22616e474db1cd4
ne-PobiH <dimass232@inbox.ru>
2026-06-03 01:32:37 +0300

    update

---
+++ kubejs/startup_scripts/main.js
@@ -0,0 +1,41 @@
+const DataComponents = Java.loadClass('net.minecraft.core.component.DataComponents')
+const BuiltInRegistries = Java.loadClass('net.minecraft.core.registries.BuiltInRegistries')
+
+const n = 2
+
+function changeFood(event, id, nutrition, saturationModifier) {
+  event.modify(id, item => {
+    item.modifyFood(food => {
+      food.nutrition(nutrition)
+      food.saturation(saturationModifier)
+    })
+  })
+}
+
+ItemEvents.modification(event => {
+  const iterator = BuiltInRegistries.ITEM.iterator()
+
+  let mcItem
+  let oldFood
+  let id
+  let newNutrition
+  let newSaturation
+  let newSaturationModifier
+
+  while (iterator.hasNext()) {
+    mcItem = iterator.next()
+    oldFood = mcItem.components().get(DataComponents.FOOD)
+
+    if (oldFood == null) {
+      continue
+    }
+
+    id = BuiltInRegistries.ITEM.getKey(mcItem).toString()
+
+    newNutrition = Math.max(1, Math.floor(oldFood.nutrition() / n))
+    newSaturation = Math.floor(oldFood.saturation() / n)
+    newSaturationModifier = newSaturation / (newNutrition * 2)
+
+    changeFood(event, id, newNutrition, newSaturationModifier)
+  }
+})
